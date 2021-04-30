ba4a5380f4aea8b3e17c695230c7bd150ada9b96
CarolineSiqueira <santos.carolinesiqueira@gmail.com>
2021-04-30 20:45:27 -0300

    Inclui JavaScript e as imagens

---
+++ etapas.js
@@ -0,0 +1,54 @@
+let etapas = [
+    {
+        titulo:'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+                numero:38111,
+                nome: 'Maria Lúcia',
+                partido:'ABC',
+                fotos:[
+                    {url:'1.png', legenda:'Vereador'}
+                ]
+                
+            },
+            {
+                numero:38222,
+                nome: 'Jeremias',
+                partido:'DEF',
+                fotos:[
+                    {url:'7.png', legenda:'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros:2,
+        candidatos:[
+            {
+                numero:77,
+                nome:'João de Santo Cristo',
+                vice:'Ciclano',
+                partido:'FGH',
+                fotos:[
+                    {url:'9.png',legenda: 'Prefeito'},
+                    {url:'2.png', legenda: 'Vice-Prefeito',small:true}
+                ]
+
+            },
+            {
+                numero:55,
+                nome:'Pablo',
+                vice:'Beltrana',
+                partido:'IJL',
+                fotos:[
+                    {url:'10.png',legenda: 'Prefeito'},
+                    {url:'12.png', legenda: 'Vice-Prefeito',small:true}
+                ]
+
+            }
+
+        ]
+    }
+]
