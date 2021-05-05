9fc34be8aa1fc0dbbc2a3d97793b4a83a14ddeae
CarolineSiqueira <santos.carolinesiqueira@gmail.com>
2021-05-05 19:46:10 -0300

    Finaliza projeto

--- etapas.js
+++ etapas.js
@@ -4,7 +4,7 @@ let etapas = [
         numeros: 5,
         candidatos:[
             {
-                numero:38111,
+                numero:'38111',
                 nome: 'Maria Lúcia',
                 partido:'ABC',
                 fotos:[
@@ -13,7 +13,7 @@ let etapas = [
                 
             },
             {
-                numero:38222,
+                numero:'38222',
                 nome: 'Jeremias',
                 partido:'DEF',
                 fotos:[
@@ -27,7 +27,7 @@ let etapas = [
         numeros:2,
         candidatos:[
             {
-                numero:77,
+                numero:'77',
                 nome:'João de Santo Cristo',
                 vice:'Ciclano',
                 partido:'FGH',
@@ -38,12 +38,12 @@ let etapas = [
 
             },
             {
-                numero:55,
+                numero:'55',
                 nome:'Pablo',
                 vice:'Beltrana',
                 partido:'IJL',
                 fotos:[
-                    {url:'10.png',legenda: 'Prefeito'},
+                    {url:'13.png',legenda: 'Prefeito'},
                     {url:'12.png', legenda: 'Vice-Prefeito',small:true}
                 ]
 
--- script.js
+++ script.js
@@ -7,13 +7,21 @@ let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
 let numero = '';
+let votoBranco = true;
 
 function comecarEtapa() {
     let etapa = etapas[etapaAtual];
 
-    let numeroHTML = '';
-    for(let i=0;i<etapa.numeros;i++) {
-        numeroHTML += '<div class="numero"></div>';
+    let numeroHtml = '';
+    numero = '';
+    votoBranco = false;
+
+    for(let i=0; i<etapa.numeros; i++) {
+        if(i === 0) {
+        numeroHtml += '<div class="numero pisca"></div>';
+        } else {
+            numeroHtml += '<div class="numero "></div>';
+        }
     }
 
     seuVotoPara.style.display = 'none';
@@ -21,36 +29,94 @@ function comecarEtapa() {
     descricao.innerHTML = '';
     aviso.style.display = 'none';
     lateral.innerHTML = '';
-    numeros.innerHTML = numeroHTML;
+    numeros.innerHTML = numeroHtml;
 
 }
 
 function atualizaInterface() {
-    
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item) =>{
+        if(item.numero === numero) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    if(candidato.length > 0) {
+        candidato = candidato [0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/> Partido: ${candidato.partido}`;
+
+        let fotosHtml = '';
+        for (let i in candidato.fotos) {
+            fotosHtml +=  `<div class="d-1-image">
+            <img src="images/${candidato.fotos[i].url}" alt="" />
+            ${candidato.fotos[i].legenda}
+        </div>`
+        }
+        lateral.innerHTML = fotosHtml;
+    } else {
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class= "aviso-grande pisca">VOTO NULO</div>';
+
+    }
 }
 
 
 function clicou(n){
     let elNumero = document.querySelector('.numero.pisca');
     if(elNumero !== null) {
-        numero.innerHTML = n;
+        elNumero.innerHTML = n;
         numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null){
+        elNumero.nextElementSibling.classList.add('pisca');
+        } else {
+            atualizaInterface();
+        }
     }
 }
 
 function branco(){
-    alert("Clicou em BRANCO!");
-
+    if( numero === '') {
+        votoBranco = true;
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        numeros.innerHTML = '';
+        descricao.innerHTML = '<div class= "aviso-grande pisca">VOTO EM BRANCO</div>';
+    } else {
+        alert('Para VOTAR EM BRANCO, não digite nenhum número!')
+    }
 }
 
 function corrige(){
-    alert("Clicou em CORRIGE!")
-
+    comecarEtapa();
 }
 
 function confirma(){
-    alert("Clicou em CONFIRMA!")
+    let etapa = etapas[etapaAtual];
 
+    let votoConfirmado = false;
+
+    if(votoBranco === true) {
+        votoConfirmado = true;
+        console.log("Confirmando voto em branco")
+    } else if(numero.length === etapa.numeros){
+        votoConfirmado = true;
+        console.log("Confirmando como " + numero);
+    }
+
+    if (votoConfirmado) {
+        etapaAtual++;
+        if(etapas[etapaAtual] !== undefined) {
+            comecarEtapa();
+        } else {
+            document.querySelector('.tela').innerHTML='<div class= "aviso-gigante pisca">FIM</div>';
+        }
+    }
 }
 
 comecarEtapa();
